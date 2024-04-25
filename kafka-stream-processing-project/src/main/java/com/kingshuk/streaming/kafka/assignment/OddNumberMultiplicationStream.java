cba65d049930fac76dde4a86999aa3b5d5a2d534
royalbytes <45887761+discovered1086@users.noreply.github.com>
2024-04-24 20:43:21 -0500

    Kafka streaming code added

--- kafka-stream-processing-project/src/main/java/com/kingshuk/streaming/kafka/assignment/OddNumberMultiplicationStream.java
+++ kafka-stream-processing-project/src/main/java/com/kingshuk/streaming/kafka/assignment/OddNumberMultiplicationStream.java
@@ -1,4 +1,4 @@
-package com.kingshuk.streaming.kafka;
+package com.kingshuk.streaming.kafka.assignment;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.*;
@@ -9,9 +9,9 @@ import org.slf4j.LoggerFactory;
 import java.util.Properties;
 
 @SuppressWarnings("java:S2095")
-public class DataFlowStream {
+public class OddNumberMultiplicationStream {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(DataFlowStream.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(OddNumberMultiplicationStream.class);
 
     public static void main(String[] args) {
         Properties properties = new Properties();
@@ -20,21 +20,17 @@ public class DataFlowStream {
         properties.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
                 Serdes.String().getClass().getName());
         properties.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
-                Serdes.String().getClass().getName());
+                Serdes.Integer().getClass().getName());
 
         StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, String> stream = builder.stream("bharath-kafka-streams-input-topic");
+        KStream<String, Integer> stream = builder.stream("bharath-kafka-streams-input-topic");
         stream.foreach((k, v) -> LOGGER.info("The key: {}, value: {}", k, v));
-        stream.filter((k, v) -> v.contains("token"))
-                .map((k, v) -> KeyValue.pair(k, v.toUpperCase()))
+        stream.filter((k, v) -> v % 2 != 0)
+                .map((k, v) -> KeyValue.pair(k, Math.multiplyExact(v, 3)))
                 .to("bharath-kafka-streams-output-topic");
 
         Topology topology = builder.build();
         LOGGER.info("The topology is {}", topology.describe());
-
-//        try(KafkaStreams streams = new KafkaStreams(topology, properties)){
-//            streams.start();
-//        }
 
         KafkaStreams streams = new KafkaStreams(topology, properties);
         streams.start();
