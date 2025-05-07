308e440fcf0f24577f7b1f31f830b0eb33a2ae39
royalbytes <45887761+discovered1086@users.noreply.github.com>
2025-05-07 07:59:57 -0500

    Spring boot version upgrade

--- kafka-poison-pill-consumer/src/main/java/com/kingshuk/messaging/kafka/kafkapoisonpillconsumer/KafkaPoisonPillConsumerApplication.java
+++ kafka-poison-pill-consumer/src/main/java/com/kingshuk/messaging/kafka/kafkapoisonpillconsumer/KafkaPoisonPillConsumerApplication.java
@@ -1,25 +1,7 @@
 package com.kingshuk.messaging.kafka.kafkapoisonpillconsumer;
 
-import org.apache.kafka.common.serialization.ByteArrayDeserializer;
-import org.apache.kafka.common.serialization.ByteArraySerializer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
-import org.springframework.kafka.listener.LoggingErrorHandler;
-import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
-
-import java.util.Map;
 
 @SpringBootApplication
 public class KafkaPoisonPillConsumerApplication {
